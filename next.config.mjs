c472aba83d9899cddee40107b0918595a82b95af
Maverick Manasco <MaverickCER@gmail.com>
2024-02-20 05:38:01 -0500

    GDN-1 #comment allow images from cdn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'q54ouf6kgkl8kx0c.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
